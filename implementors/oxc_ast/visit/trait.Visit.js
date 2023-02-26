47f351a1bbd1047da0a76593e74a9b5c185c3672
jason89521 <jason89521@users.noreply.github.com>
2023-02-26 15:32:32 +0000

    deploy: e2207b9b1573ecc7cf6d8eb961a9947085a36a2a

--- implementors/oxc_ast/visit/trait.Visit.js
+++ implementors/oxc_ast/visit/trait.Visit.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"oxc_semantic":[["impl&lt;'a&gt; Visit&lt;'a&gt; for <a class=\"struct\" href=\"oxc_semantic/struct.SemanticBuilder.html\" title=\"struct oxc_semantic::SemanticBuilder\">SemanticBuilder</a>&lt;'a&gt;"]]
+"oxc_semantic":[["impl&lt;'a&gt; <a class=\"trait\" href=\"oxc_ast/visit/trait.Visit.html\" title=\"trait oxc_ast::visit::Visit\">Visit</a>&lt;'a&gt; for <a class=\"struct\" href=\"oxc_semantic/struct.SemanticBuilder.html\" title=\"struct oxc_semantic::SemanticBuilder\">SemanticBuilder</a>&lt;'a&gt;"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
